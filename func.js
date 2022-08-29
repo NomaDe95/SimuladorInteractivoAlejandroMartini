82e9e867e1c4f714f3522dbc9ab931e41d5a7379
Alejandro Martini <amartini829@gmail.com>
2022-08-29 01:16:30 -0300

    primer commit

---
+++ func.js
@@ -0,0 +1,13 @@
+//Valor a multiplicar con el precio base del juego
+let imp = 1.74
+//Colocar precio base del juego
+let precio = pedirPrecio()
+
+function pedirPrecio(){
+    let precioBase = parseInt(prompt("Ingrese precio"))
+    return precioBase
+}
+
+function precioTotal(){
+    
+}
